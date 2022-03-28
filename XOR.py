d8f531e8fac5dc527db0db3a4a0f289a2e016c63
L-Ermite <87863570+L-Ermite@users.noreply.github.com>
2022-03-28 22:52:20 +0200

    Update XOR.py

--- XOR.py
+++ XOR.py
@@ -1,56 +1,30 @@
-def xor_encrypt(msg, key, char=False, csv=False, debug_mode=False):
-    """IN : INT or STR, INT or STR (BINARY) , BOOL x 3 (optional)
-    OUT : STR
-    Code le msg avec la méthode XOR
-    Possède plusieurs paramètres:
-        char convertis les résultats binaires en caractères unicodes
-        csv  indique que les valeurs sont séparées par des virgules
-        debug_mode donne plusieurs information en plus du message codé"""
+def huit_bits(nb):
+    """
+    :param nb: STR -> Un nombre en bianire sous la forme d'une chaîne de caractères
+    :return: STR -> Le même nombre écris sur 8 bits
+    """
 
-    debug_data = {'original_type': type(msg), 'key_type': type(key)}  # initialisation de plusieurs variables
-    encoded_message = ''
-    encoded_message_char = ''
+    while len(nb) < 8:
+        nb = "0" + nb
 
-    if type(msg) == int:
-        msg = str(msg)
+    return nb
 
-    if csv:                                     # Formation de groupe en fonction :
-        msg = msg.split(',')                    # - des virgules qui séparaient les données
-    else:                                       #
-        msg = [bin(ord(c))[2:] for c in msg]    # - du résultat du ord() de chaque caractère du message
 
-    if type(key) == int:
-        key = bin(key)  # On convertit la clé en binaire
-    if key[1] == 'b':       # Si la clé est au format 0b...
-        key = key[2:]
+def xor_encrypt(msg, key):
+    """
+    :param msg: STR -> Une chaîne de caractère
+    :param key: STR -> Un nombre en binaire sous la forme d'une chaîne de caractères
+    :return: STR -> Le message codé par la méthode xor
+    """
 
-    debug_data['key'] = int(key, 2)     #
-    debug_data['msg'] = msg             # Création de plusieurs catégories pour le debug_mode
-    debug_data['coded_no_grp'] = ''     #
-    debug_data['grp'] = []              #
+    c_msg = ""
+    key = huit_bits(key)
 
-    ki = 0                              # Les festivités commencent :
-    for c in msg:                       # On boucle sur les groupent
-        groupping = ''
-        for i in range(len(c)):         # Puis sur les bits individuels
+    for c in msg:
+        c = huit_bits(bin(ord(c))[2:])
+        c_c = ""
+        for i in range(len(c)):
+            c_c += str(int(c[i]) ^ int(key[i]))
+        c_msg += chr(int(c_c, 2))
 
-            if ki >= len(key):          # Si jamais le message est plus long que la clé ...
-                ki = 0                  # ... on recommence au début de cette dernière
-
-            debug_data['coded_no_grp'] += str(int(key[ki]) ^ int(c[i]))+','     # Pour voir ce qui c'est passé en debug
-            groupping += str(int(key[ki]) ^ int(c[i]))                          # On reforme les groupes
-            ki += 1
-
-        encoded_message += groupping+','                 # La production finale si char est désactivé
-        debug_data['grp'].append(groupping)              # Surtout utile avec char d'activé pour voir les valeur avant chr()
-
-        if char:
-            encoded_message_char += chr(int(groupping, 2))      # On convertit en caractère Unicode
-
-    if char:                                                    # Puis on remplace l'ancien message
-        encoded_message = encoded_message_char
-
-    if debug_mode:
-        return encoded_message, debug_data
-    else:
-        return encoded_message
+    return c_msg
